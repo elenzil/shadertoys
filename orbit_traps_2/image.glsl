8d45f3ad99f837c40db2c0e3d881b7bb320343f0
Orion Elenzil <dococelot@gmail.com>
2019-04-09 22:00:20 -0500

    points of interest

--- orbit_traps_2/image.glsl
+++ orbit_traps_2/image.glsl
@@ -13,21 +13,38 @@ vec2 complexMul(in vec2 A, in vec2 B) {
   return vec2((A.x * B.x) - (A.y * B.y), (A.x * B.y) + (A.y * B.x));
 }
 
+struct POI {
+  vec2  center;
+  float range;
+  float maxIter;
+};
+
+const POI[] pointsOfInterest = POI[] (
+    // the main image
+    POI(vec2(-0.75    ,  0.0      ), 1.4     ,  200.0),
+
+    // Spirals from Paul Bourke http://paulbourke.net/fractals/mandelbrot
+    POI(vec2(-0.761571, -0.084756),  0.000012, 2000.0),
+
+    // nowhere special
+    POI(vec2(-1.4076  , -0.1277   ), 0.00014 , 3000.0)
+  );
 
 void mainImage(out vec4 outRGBA, in vec2 XY)
 {
   float smallWay = min(iResolution.x, iResolution.y);
   vec2 UV = (XY * 2.0 - fv2_1 * iResolution.xy)/smallWay;
 
-  vec2 C = UV * 1.4 - 0.65 * fv2_x; // + offset stuff from bufferA
+  POI poi = pointsOfInterest[1];
+
+  vec2 C = UV * poi.range + poi.center;
   vec2 Z = C;
 
-  float maxIter = 100.0;
-  float numIter =   0.0;
-  for (; numIter < maxIter; ++numIter) {
-    Z  = complexMul(Z, Z);
-    Z += C;
-    if (dot(Z, Z) > 16.0) {
+  float maxIter = poi.maxIter;
+  float numIter;
+  for (numIter = 0.0; numIter < maxIter; ++numIter) {
+    Z  = complexMul(Z, Z) + C;
+    if (dot(Z, Z) > 4.0) {
       break;
     }
   }
