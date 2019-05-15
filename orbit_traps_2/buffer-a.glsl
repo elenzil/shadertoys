c8d65d1eecc9100190c5bea8e7948cbbeb0d662b
Orion Elenzil <dococelot@gmail.com>
2019-05-14 20:54:50 -0700

    yep

--- orbit_traps_2/buffer-a.glsl
+++ orbit_traps_2/buffer-a.glsl
@@ -4,6 +4,7 @@
 // (0, 0) = most recent iMouse.
 // (1, 0) = the iMouse before that.
 // (2, 0) = Point Of Interest. (POI struct)
+// (3, 0) = iMouse when the button became pressed.
 
 struct POI {
   vec2  center;
@@ -22,22 +23,22 @@ const POI[] pointsOfInterest = POI[] (
   POI(vec2(-1.4076  , -0.1277   ), 0.00014 , 3000.0)
 );
 
-vec4 poiToVec4(in POI poi) {
-  return vec4(poi.center, poi.range, poi.maxIter);
-}
-
-POI vec4ToPOI(in vec4 v) {
-  return POI(v.xy, v.z, v.w);
-}
+vec4 poiToVec4(in POI poi) {return vec4(poi.center, poi.range, poi.maxIter);}
+POI vec4ToPOI(in vec4 v) {return POI(v.xy, v.z, v.w);}
 
 void mainImage(out vec4 outRGBA, in vec2 XY)
 {
+  ivec2 IJ = ivec2(XY);
+
   if (iFrame == 0) {
-    outRGBA = vec4(0.0, 0.0, 0.0, 0.0);
+    if (IJ.x == 2) {
+      outRGBA = poiToVec4(pointsOfInterest[0]);
+    }
+    else {
+      outRGBA = vec4(0.0, 0.0, 0.0, 0.0);
+    }
     return;
   }
-
-  ivec2 IJ = ivec2(XY);
 
   if (IJ.y > 0) {
     // don't care
@@ -52,6 +53,23 @@ void mainImage(out vec4 outRGBA, in vec2 XY)
     outRGBA = texelFetch(iChannel0, ivec2(IJ.x - 1, IJ.y), 0);
   }
   else {
+    vec4 prevIMouse = texelFetch(iChannel0, ivec2(0, 1), 0);
+    bool wasPressed = (prevIMouse.z + prevIMouse.w) > 0.5;
+    bool nowPressed = (    iMouse.z +     iMouse.w) > 0.5;
+    bool justBecamePressed = nowPressed && !wasPressed;
+
+    if ((IJ.x == 3) && justBecamePressed) {
+      outRGBA = iMouse;
+    }
+    else if (IJ.x == 2) {
+      vec2 mouseDelta = iMouse.xy - texelFetch(iChannel0, ivec2(3, 0), 0).xy;
+      POI poi = vec4ToPOI(texelFetch(iChannel0, IJ, 0));
+      poi.center += mouseDelta;
+      outRGBA = poiToVec4(poi);
+    }
+    else {
+      outRGBA = texelFetch(iChannel0, IJ, 0);
+    }
   }
 }
 
--- orbit_traps_2/image.glsl
+++ orbit_traps_2/image.glsl
@@ -20,6 +20,9 @@ struct POI {
   float range;
   float maxIter;
 };
+vec4 poiToVec4(in POI poi) {return vec4(poi.center, poi.range, poi.maxIter);}
+POI vec4ToPOI(in vec4 v) {return POI(v.xy, v.z, v.w);}
+
 
 float mandelEscapeIters(in vec2 C, in float maxIters) {
   vec2 Z = C;
@@ -32,17 +35,6 @@ float mandelEscapeIters(in vec2 C, in float maxIters) {
   return maxIters;
 }
 
-const POI[] pointsOfInterest = POI[] (
-  // the main image
-  POI(vec2(-0.75    ,  0.0      ), 1.4     ,  200.0),
-
-  // Spirals from Paul Bourke http://paulbourke.net/fractals/mandelbrot
-  POI(vec2(-0.761571, -0.084756),  0.000012, 2000.0),
-
-  // nowhere special
-  POI(vec2(-1.4076  , -0.1277   ), 0.00014 , 3000.0)
-);
-
 void mainImage(out vec4 outRGBA, in vec2 XY)
 {
   float smallWay = min(iResolution.x, iResolution.y);
@@ -50,16 +42,8 @@ void mainImage(out vec4 outRGBA, in vec2 XY)
 
   float t = iTime * 0.1;
 
-  POI poi1 = pointsOfInterest[0];
-  POI poi2 = pointsOfInterest[int(t) % (3 - 1) + 1];
-
-  float mixAmt = sin(t * TWOPI - PI/2.0) * 0.5 + 0.5;
-  mixAmt = pow(mixAmt, 0.3);
-  POI poi = POI(
-    mix(poi1.center , poi2.center , mixAmt),
-    mix(poi1.range  , poi2.range  , mixAmt),
-    mix(poi1.maxIter, poi2.maxIter, mixAmt));
-
+  POI poi = vec4ToPOI(texelFetch(iChannel0, ivec2(2, 0), 0));
+  
   vec2 C = uv * poi.range + poi.center;
   float f = mandelEscapeIters(C, poi.maxIter) / poi.maxIter;
   f = pow(f, 1.0/3.0);
