5a9baab09fb74c4770a3e252ec6cac7de5513ca0
Orion Elenzil <dococelot@gmail.com>
2019-05-18 02:05:31 -0700

    tweaks

--- tea/image.glsl
+++ tea/image.glsl
@@ -3,6 +3,15 @@
     first marcher with shadows.
 
     click on the far right to show the marching steps.
+
+    todo:
+    * materials!
+    * reflections
+    * improved shadows
+    * worry about efficiency of gradient()
+    * fisheye camera
+    * AA
+    * AO
 */
 
 // boiler-plate
@@ -19,7 +28,7 @@ const float PI      = 3.14159265359;
 const float TAU     = PI * 2.0;
 
 const float rmMaxSteps  = 180.0;
-const float rmMaxDist   =  52.0;
+const float rmMaxDist   =  32.0;
 const float rmEpsilon   =   0.002;
 const float rmWhoaThere =   1.0;
 const float grEpsilon   =   0.01;
@@ -40,6 +49,10 @@ float sdfSphere(in vec3 p, in vec3 c, float r) {
     return length(p - c) - r;
 }
 
+float sdfColumn(in vec3 p, in vec3 c, float r) {
+    return length(p.xz - c.xz) - r;
+}
+
 float sdfScene(in vec3 p) {
     float dist = 1e20;
     float t    = iTime * TAU / 30.0;
@@ -50,11 +63,11 @@ float sdfScene(in vec3 p) {
     dist = min(dist, sdfSphere(vec3(abs(p.x), p.y, abs(p.z)), vec3(sep, rad, sep), rad));
     
     // minus a wavy plane
-    float cutPlaneSize = (sin(t * 0.72) * 0.51 + 0.5) * rad * 2.4;
+    float cutPlaneSize = (sin(t * 0.72 - PI/2.0) * 0.51 + 0.5) * rad * 2.4;
     float cutPlaneDisp = (sin(p.x * 4.0) + sin(p.z * 4.0)) * 0.1;
     cutPlaneDisp += sin(length(p.xz) * 20.0 - t * 20.0) * 0.015;
-    float cutPlane = max((p.y + cutPlaneDisp) - rad - cutPlaneSize * 0.5, -(p.y + cutPlaneDisp) + rad - cutPlaneSize * 0.5);
-
+    float cutPlane = max((p.y + cutPlaneDisp) - rad - cutPlaneSize * 0.5, -(p.y + cutPlaneDisp) + rad - cutPlaneSize * 0.5);    
+//  cutPlane = max(-sdfColumn(vec3(abs(p.x), p.y, abs(p.z)), vec3(sep, rad, sep), rad * 0.03), cutPlane);
     dist = max(-cutPlane, dist);
 
     // ground
@@ -94,9 +107,9 @@ void mainImage(out vec4 RGBA, in vec2 XY)
 
     float mouseAlt = iMouse.y < 1.0 ? 0.0 : iMouse.y / iResolution.y * 2.0 - 1.0;
 
-    vec3  ro = vec3(vec2(cos(t), sin(t)) * 30.0, 7.0 + mouseAlt * 6.0).xzy;
-    vec3  lt = vec3(0.0, 2.0, 0.0);
-    float zm = 10.0;
+    vec3  ro = vec3(vec2(cos(t), sin(t)) * 13.0, 7.0 + mouseAlt * 5.0).xzy;
+    vec3  lt = vec3(0.0, 1.0, 0.0);
+    float zm = 4.0;
     vec3  rd = rayDir(uv, ro, lt, fv3_y, zm);
 
     float hitDist, steps, minDist;
@@ -110,34 +123,41 @@ void mainImage(out vec4 RGBA, in vec2 XY)
 
     float b = max(0.0, lgtDot);
 
+    if (hitPoint.y < rmEpsilon * 1.1) {
+        // floor
+        float check = float(fract(hitPoint.x / 5.0) > 0.5 ^^ fract(hitPoint.z / 5.0) > 0.5);
+        b *= check * 0.1 + 0.9;
+    }
+
     if (lgtDot > 0.0) {
         // check for shadows
         float hitDist2, steps2, minDist2;
-        // back away from the surface by a little more than epsilon.
-        float softShadowSize = 0.1;
-        float backAway = max(rmEpsilon * 1.1, 0.2);
-        march(hitPoint + normal * backAway, -lgtDir, hitDist2, steps2, minDist2);
+        float backAway = rmEpsilon * 1.1;
+        float advance  = 0.2;
+        vec3  startPnt = hitPoint + normal * backAway + advance * -lgtDir;
+        march(startPnt, -lgtDir, hitDist2, steps2, minDist2);
         float shadowAmt = float(hitDist2 > rmMaxDist * 0.9);
-        shadowAmt *= smoothstep(rmEpsilon, softShadowSize, minDist2);
-        shadowAmt = shadowAmt * 0.6 + 0.4;
+        shadowAmt *= smoothstep(rmEpsilon, 0.1, minDist2);
+        shadowAmt = shadowAmt * 0.9 + 0.1;
         b *= shadowAmt;
         steps += steps2;
     }
 
     vec3  rgb    = vec3(b);
 
-    float fogStart = 25.0;
+    // fog
+    float fogStart = 6.0;
     float fogAmt = clamp((hitDist - fogStart) / (rmMaxDist - fogStart), 0.0, 1.0);
-    fogAmt = sqrt(fogAmt);
     vec3  fogClr = vec3(0.0, 0.07, 0.2);
-
     rgb = mix(rgb, fogClr, fogAmt);
 
+    // gamma
     rgb = pow(rgb, vec3(0.4545));
 
 
     if (iMouse.z > iResolution.x * 0.9) {
-        rgb.r = (steps)/2.0/rmMaxSteps;
+        rgb   *= 0.5;
+        rgb.r += (steps)/2.0/rmMaxSteps * 0.75;
     }
 
     RGBA.rgb = rgb;
