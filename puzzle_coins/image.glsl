8bdb4c165f1752b398ed960ab556fa1e76d74323
Orion Elenzil <dococelot@gmail.com>
2019-05-30 21:55:40 -0700

    numbers

--- puzzle_coins/image.glsl
+++ puzzle_coins/image.glsl
@@ -1,6 +1,6 @@
 // orion elenzil 2019028
 //
-// puzzle from mike plotz.
+// puzzle from mike plotz: https://hyponymo.us
 //
 // you have a bunch of uniform coins
 // and a long skinny box.
@@ -53,6 +53,50 @@ float sdfRect(in vec2 p, in vec2 r) {
         + min(max(d.x, d.y), 0.0);
 }
 
+//-----------------------------------------------------------------
+// Digit drawing function by P_Malin (https://www.shadertoy.com/view/4sf3RN)
+
+float SampleDigit(const in float n, const in vec2 vUV)
+{       
+    if(vUV.x  < 0.0) return 0.0;
+    if(vUV.y  < 0.0) return 0.0;
+    if(vUV.x >= 1.0) return 0.0;
+    if(vUV.y >= 1.0) return 0.0;
+    
+    float data = 0.0;
+    
+         if(n < 0.5) data = 7.0 + 5.0*16.0 + 5.0*256.0 + 5.0*4096.0 + 7.0*65536.0;
+    else if(n < 1.5) data = 2.0 + 2.0*16.0 + 2.0*256.0 + 2.0*4096.0 + 2.0*65536.0;
+    else if(n < 2.5) data = 7.0 + 1.0*16.0 + 7.0*256.0 + 4.0*4096.0 + 7.0*65536.0;
+    else if(n < 3.5) data = 7.0 + 4.0*16.0 + 7.0*256.0 + 4.0*4096.0 + 7.0*65536.0;
+    else if(n < 4.5) data = 4.0 + 7.0*16.0 + 5.0*256.0 + 1.0*4096.0 + 1.0*65536.0;
+    else if(n < 5.5) data = 7.0 + 4.0*16.0 + 7.0*256.0 + 1.0*4096.0 + 7.0*65536.0;
+    else if(n < 6.5) data = 7.0 + 5.0*16.0 + 7.0*256.0 + 1.0*4096.0 + 7.0*65536.0;
+    else if(n < 7.5) data = 4.0 + 4.0*16.0 + 4.0*256.0 + 4.0*4096.0 + 7.0*65536.0;
+    else if(n < 8.5) data = 7.0 + 5.0*16.0 + 7.0*256.0 + 5.0*4096.0 + 7.0*65536.0;
+    else if(n < 9.5) data = 7.0 + 4.0*16.0 + 7.0*256.0 + 5.0*4096.0 + 7.0*65536.0;
+    
+    vec2 vPixel = floor(vUV * vec2(4.0, 5.0));
+    float fIndex = vPixel.x + (vPixel.y * 4.0);
+    
+    return mod(floor(data / pow(2.0, fIndex)), 2.0);
+}
+
+float PrintInt(const in vec2 uv, const in float value )
+{
+    float res = 0.0;
+    float maxDigits = 1.0+ceil(log2(value + 0.00001)/log2(10.0));
+    float digitID = floor(uv.x);
+    if( digitID>0.0 && digitID<maxDigits )
+    {
+        float digitVa = mod( floor( value/pow(10.0,maxDigits-1.0-digitID) ), 10.0 );
+        res = SampleDigit( digitVa, vec2(fract(uv.x), uv.y) );
+    }
+
+    return res; 
+}
+
+
 float theta(float time) {
     return clamp(time - 3.0, 0.0, 1.0) * thetaMax;
 }
@@ -71,31 +115,40 @@ void drawBox(inout vec3 rgb, in vec2 p) {
     rgb = mix(rgb, vec3(0.0), 0.5 * smoothstep(0.1, -0.1, rd / sf) * grid);
 }
 
-void drawC(inout vec3 rgb, in vec2 p, in vec2 c, in vec3 col) {
+void drawC(inout vec3 rgb, in vec2 p, in vec2 c, in vec3 col, in float num) {
     float d  = length(p - c);
     float ce = smoothstep( 1.0, -1.0,     (d - 0.5)  / sf);
     float cb = smoothstep( 1.0, -1.0,     (d - 0.5)  / sf + coinBorderPx);
     vec3 tmp = mix(colCoinEdge, col * (d * 0.3 + 0.7), cb);
     rgb      = mix(rgb, tmp, ce * 0.7);
+    rgb *= 1.0 - 0.5 * PrintInt((p - c + vec2(0.47, 0.1)) * 6.0, num);
 }
 
 void drawC1(inout vec3 rgb, in vec2 p, in mat2 rot, in float dxMod) {
     if (p.x < 0.0 || p.x > 1000.0) {
         return;
     }
-    p.x = mod(p.x, 2.0 + dxMod);
+
+    float step = 2.0 + dxMod;
+    float num = 1.0 + 4.0 * floor(p.x / step);
+
+    p.x = mod(p.x, step);
     vec2 c = vec2(0.5, -0.5);
     c -= vec2(0.5, -0.5);
     c *= rot;
     c += vec2(0.5, -0.5);
-    drawC(rgb, p, c, colCoinBody1);
+    drawC(rgb, p, c, colCoinBody1, num);
 }
 void drawC2(inout vec3 rgb, in vec2 p, in mat2 rot, in float dxMod) {
     float dx = clamp(t - 1.0, 0.0, 1.0) * 0.5;
     if (p.x < 0.0 || p.x > 1000.0) {
         return;
     }
-    p.x = mod(p.x, 2.0 + dxMod);
+
+    float step = 2.0 + dxMod;
+    float num = 2.0 + 4.0 * floor((p.x - dx) / step);
+
+    p.x = mod(p.x, step);
     vec2 c = vec2(0.5, 0.5);
     c.x += dx;
     float dy = 1.0 - sqrt(1.0 - dx * dx);
@@ -104,26 +157,34 @@ void drawC2(inout vec3 rgb, in vec2 p, in mat2 rot, in float dxMod) {
     c *= rot;
     c += vec2(0.5, -0.5);
 
-    drawC(rgb, p, c, colCoinBody2);
+    drawC(rgb, p, c, colCoinBody2, num);
 }
 void drawC3(inout vec3 rgb, in vec2 p, in mat2 rot, in float dxMod) {
     if (p.x < 0.0 || p.x > 1000.0) {
         return;
     }
-    p.x = mod(p.x + dxMod, 2.0 + dxMod) - dxMod;
+
+    float step = 2.0 + dxMod;
+    float num = 3.0 + 4.0 * floor(p.x / step);
+
+    p.x = mod(p.x + dxMod, step) - dxMod;
     vec2 c = vec2(1.5, -0.5);
     c -= vec2(0.5, -0.5);
     c *= rot;
     c += vec2(0.5, -0.5);
 
-    drawC(rgb, p, c, colCoinBody3);
+    drawC(rgb, p, c, colCoinBody3, num);
 }
 void drawC4(inout vec3 rgb, in vec2 p, in mat2 rot, in float dxMod) {
     float dx = clamp(t - 1.0, 0.0, 1.0) * 0.5;
     if (p.x < 0.5 || p.x > 1000.0) {
         return;
     }
-    p.x = mod(p.x - dx, 2.0 + dxMod) + dx;
+
+    float step = 2.0 + dxMod;
+    float num = 4.0 + 4.0 * floor((p.x - dx) / step);
+
+    p.x = mod(p.x - dx, step) + dx;
     vec2 c = vec2(1.5, 0.5);
     c.x += dx;
     float dy = 1.0 - sqrt(1.0 - dx * dx);
@@ -132,7 +193,7 @@ void drawC4(inout vec3 rgb, in vec2 p, in mat2 rot, in float dxMod) {
     c *= rot;
     c += vec2(0.5, -0.5);
 
-    drawC(rgb, p, c, colCoinBody4);
+    drawC(rgb, p, c, colCoinBody4, num);
 }
 
 void render(out vec4 RGBA, in vec2 XY, bool showLeft) {
@@ -142,17 +203,24 @@ void render(out vec4 RGBA, in vec2 XY, bool showLeft) {
     t = 0.0;
     #endif
 
+    float vign = 1.0;
+
     vec2 uv;
     /**/  sf = gViewPort / (iResolution.y * 0.5);
     if (showLeft) {
-        sf    = mix (sf, sf * 0.1, 2.0 * clamp(t - 4.5, 0.0, 0.5));
+        float tMe = clamp(t - 4.5, 0.0, 0.5);
+     //   tMe=0.5;
+        sf    = mix (sf, sf * 0.1, 2.0 * tMe);
         uv    = (XY - iResolution.xy * vec2(0.0, 0.25)) * sf;
-        uv.x -= mix(0.25, iResolution.x * sf * 0.5 - 2.0, 2.0 * clamp(t - 4.5, 0.0, 0.5));
-        uv.y -= mix(0.0 , 0.5, 2.0 * clamp(t - 4.5, 0.0, 0.5));
+        uv.x -= mix(0.25, iResolution.x * sf * 0.5 - 2.0, 2.0 * tMe);
+        uv.y -= mix(0.0 , 0.5, 2.0 * tMe);
+
+        vign  = 1.0 - tMe * 2.0 * (0.8 * smoothstep(0.0, 1.0, 4.0 * length(uv - vec2(2.0, -0.5))));
     }
     else {
         uv    = (XY - iResolution.xy * vec2(0.0, 0.25)) * sf;
         uv.x += 1000.5 - iResolution.x * sf;
+        vign  = 1.0;
     }
 
     vec3 rgb = bg;
@@ -171,12 +239,14 @@ void render(out vec4 RGBA, in vec2 XY, bool showLeft) {
     drawC3 (rgb, uv, rot, dxMod);
     drawC4 (rgb, uv, rot, dxMod);
 
+    rgb *= vign;
+
     // fade out
     if (showLeft) {
-        rgb = mix(rgb, bg, smoothstep(-3.0, 3.0, XY.x - iResolution.x + 50.0 - sin(XY.y * 0.03) * 20.0));
+        rgb = mix(rgb, bg, smoothstep(-3.0, 3.0, XY.x - iResolution.x + 30.0 - sin(XY.y * 50.0 / iResolution.y) * 1500.0 / iResolution.y));
     }
     else {
-        rgb = mix(rgb, bg, smoothstep(-3.0, 3.0, -XY.x + 50.0 + sin(XY.y * 0.03) * 20.0));
+        rgb = mix(rgb, bg, smoothstep(-3.0, 3.0, -XY.x + 20.0 + sin(XY.y * 50.0 / iResolution.y) * 1500.0 / iResolution.y));
     }
 
     RGBA.rgb = pow(rgb, vec3(0.4545));
