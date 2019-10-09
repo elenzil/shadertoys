0a3f1b59bf78d88956f13086048d329a0b92a8d3
Orion Elenzil <dococelot@gmail.com>
2019-10-08 21:46:35 -0700

    different floor material.

--- bh1/image.glsl
+++ bh1/image.glsl
@@ -1,3 +1,12 @@
+/*
+first shader w/ materials.
+known issues:
+  * no AA
+  * very limited reflection
+  * shading model not so great
+  * scintillating edges
+*/
+
 // common stuff
 const vec3  fv3_1   = vec3(1.0, 1.0, 1.0);
 const vec3  fv3_0   = vec3(0.0, 0.0, 0.0);
@@ -14,13 +23,14 @@ const float MAX_FLOAT = intBitsToFloat(0x7f7fffff);
 
 
 // less common
-const float rmMaxSteps = 150.0;
+const float rmMaxSteps = 400.0;
 const float rmMaxDist  = 150.0;
-const float rmEpsilon  =   0.01;
-const float grEpsilon  =   0.001;
+const float rmEpsilon  =   0.001;
+const float grEpsilon  =   0.00001;
+const float nrmBackoff =   grEpsilon * 10.0;
 
-const vec3  fogColor   = vec3(0.0);
-const float shadowFac  = 0.02;           // 1.0 for no shadows, 0.0 for black.
+const float shadowFac  = 0.1;           // 1.0 for no shadows, 0.0 for black.
+const vec3  clr_fog   = vec3(shadowFac);
 
 float gT = 0.0;
 
@@ -31,15 +41,16 @@ float gT = 0.0;
 #define mat_3       3
 #define mat_4       4
 
-vec3 colors[7] = vec3[7] (
-    vec3(1.0, 1.0, 0.0),          // yellowish
-    vec3(0.5, 1.0, 1.0),          // cyanish
-    vec3(0.4, 0.1, 0.1),          // reddish
-    vec3(0.7, 0.0, 0.1),          // magenta
-    vec3(0.1, 0.9, 0.3),          // greenish
-    vec3(1.0, 0.5, 0.7),          // pinkish
-    vec3(0.8, 0.8, 0.8)           // whitish
-);
+#define clr_cyn vec3(0.5, 0.8, 0.8)
+#define clr_er1 vec3(1e3, 0e0, 0e0)
+#define clr_er2 vec3(1e3, 0e0, 1e3)
+#define clr_er3 vec3(0e0, 0e0, 1e3)
+#define clr_grn vec3(0.1, 0.9, 0.3)
+#define clr_mag vec3(0.7, 0.0, 0.1)
+#define clr_pnk vec3(1.0, 0.5, 0.7)
+#define clr_red vec3(0.4, 0.2, 0.2)
+#define clr_wht vec3(0.8, 0.8, 0.8)
+#define clr_yel vec3(1.0, 1.0, 0.0)
 
 struct SurfaceHit {
     int  material;
@@ -60,19 +71,23 @@ float checkers2D(vec2 p)
 vec3 albedo(in SurfaceHit sh) {
     switch(sh.material) {
         default:
-            return vec3(0e0, 0e0, 1e3);
+            return clr_er1;
         case mat_no_hit:
-            return vec3(1e3, 0e0, 1e3);
+            return clr_fog;
         case mat_0:
-            return colors[4] * (0.7 + 0.3 * checkers2D(vec2(sh.position.y - gT * 3.0, 0.1) * 1.0));
+            return clr_pnk * (0.7 + 0.3 * checkers2D(vec2(sh.position.y - gT * 3.0, 0.1) * 1.0));
         case mat_1:
-            return colors[5] * (0.7 + 0.3 * checkers2D(vec2(sh.position.y + gT * 3.0, 0.1) * 1.0));
-        case mat_2:
-            return mix(colors[1], colors[2], checkers2D(sh.position.xz * 0.1));
+            return clr_grn * (0.7 + 0.3 * checkers2D(vec2(sh.position.y - gT * 3.0, 0.1) * 1.0));
+        case mat_2: {
+            float w = 1.0/(2.0 + dot(sh.position.xz, sh.position.xz));
+            vec3 c1 = mix(clr_yel, clr_wht, smoothstep(0.5 - w, 0.5 + w, 2.0 * abs(-0.5 + fract(0.25 + 16.0 * (atan(sh.position.x, sh.position.z) / TAU)))));
+            vec3 c2 = mix(clr_red, clr_cyn, checkers2D(sh.position.xz * 0.1));
+            return mix(c1, c2, clamp(length(sh.position.xz) / (rmMaxDist * 0.5) + 0.2, 0.0, 1.0));
+          }
         case mat_3:
-            return colors[3];
+            return clr_mag * (0.7 + 0.3 * checkers2D(vec2(sh.position.y + gT * 3.0, 0.1) * 1.0));
         case mat_4:
-            return colors[6];
+            return clr_wht;
     }
 }
 
@@ -105,19 +120,24 @@ float sdf(in vec3 p, out int material) {
   float D;
   int   m = mat_no_hit;
 
-  D = sdfSphere(p + fv3_x * -0.3 * sphRad,  sphRad);
+  D = sdfSphere(p,  sphRad);
   d = min(d, D);
   m = d == D ? mat_0 : m;
 
-  D = sdfSphere(p + fv3_x * +0.3 * sphRad,  sphRad);
-  d = max(d, 1.0-D);
-  m = d == 1.0-D ? mat_1 : m;
+  const float offset = 1.1;
+  D = sdfSphere(p + fv3_x * +offset * sphRad,  sphRad);
+  d = max(d, -D);
+  m = d == -D ? mat_1 : m;
 
-  D = sdfCylinderX(p, sphRad * 0.25);
-  d = max(d, 1.0-D);
-  m = d == 1.0-D ? mat_3 : m;
+  D = sdfSphere(p + fv3_x * -offset * sphRad,  sphRad);
+  d = max(d, -D);
+  m = d == -D ? mat_1 : m;
 
-  D = sdfSphere(p + fv3_x * -0.9 * sphRad,  sphRad * 0.125);
+  D = sdfCylinderX(p, sphRad * 0.5);
+  d = max(d, -D);
+  m = d == -D ? mat_3 : m;
+
+  D = sdfSphere(p,  sphRad * 0.2);
   d = min(d, D);
   m = d == D ? mat_4 : m;
 
@@ -164,53 +184,65 @@ SurfaceHit march(in vec3 p, in vec3 rd, out float numSteps) {
 
 void mainImage(out vec4 RGBA, in vec2 XY)
 {
-  RGBA.a   = 1.0;
+    RGBA.a   = 1.0;
 
-  float smallWay = min(iResolution.x, iResolution.y);
-  vec2  uv = (XY * 2.0 - fv2_1 * iResolution.xy)/smallWay;
-  gT  = iTime * TAU * 0.01;
-  vec3  ro = vec3( vec2(cos(gT), sin(gT)) * 30.0, 12.0).xzy;
-  vec3  la = vec3( 0.0, 0.0, 0.0);
-  float zoom = 3.2;
-  vec3  rd = getRayDirection(ro, la, uv, zoom);
+    float smallWay = min(iResolution.x, iResolution.y);
+    vec2  uv = (XY * 2.0 - fv2_1 * iResolution.xy)/smallWay;
+    gT  = iTime * TAU * 0.01;
+    vec3  ro = vec3( vec2(cos(gT), sin(gT)) * 30.0, mix(8.0, 20.0, sin(gT) * 0.5 + 0.5)).xzy;
+    vec3  la = vec3( 0.0, 0.0, 0.0);
+    float zoom = 4.0;
+    vec3  rd = getRayDirection(ro, la, uv, zoom);
 
-  vec3  rgb = fogColor;
+    vec3  rgb = clr_fog;
 
-  vec3  ld = normalize(vec3(sin(gT * -3.0), 2.0 * (sin(gT * 5.0) * 0.49 + 0.51), cos(gT * -3.0)));
+    vec3  ld = normalize(vec3(sin(gT * -3.0), 2.0 * (sin(gT * 5.0) * 0.49 + 0.51), cos(gT * -3.0)));
 
-  float numSteps;
-  SurfaceHit sh = march(ro, rd, numSteps);
-  if (sh.material != mat_no_hit) {
+    float numSteps;
+    SurfaceHit sh = march(ro, rd, numSteps);
     rgb = albedo(sh);
 
-    vec3 nrm = estimateNormal(sh.position);
+    vec3 nrm = estimateNormal(sh.position - rd * nrmBackoff);
+
+    float unused;
+
+    if (sh.material == mat_2) {
+      SurfaceHit shrf = march(sh.position + nrm * 0.01, reflect(rd, nrm), unused);
+      rgb = mix(rgb, albedo(shrf), 0.2);
+    }
+    if (sh.material == mat_4) {
+      SurfaceHit shrf = march(sh.position + nrm * 0.01, reflect(rd, nrm), unused);
+      rgb = mix(rgb, albedo(shrf), 0.2);
+    }
 
     // shading
     float shadeFac = max(shadowFac, dot(ld, nrm));
 
     // shadow
-    float unused;
-    SurfaceHit shsh = march(sh.position + nrm * 0.1, ld, unused);
+    SurfaceHit shsh = march(sh.position + nrm * 0.01, ld, unused);
     if (shsh.material != mat_no_hit) {
-      shadeFac = shadowFac;
+        shadeFac = shadowFac;
     }
 
     rgb *= shadeFac;
 
     // fog
     float dist = length(ro - sh.position);
-    rgb = mix(rgb, fogColor, clamp(dist/rmMaxDist - 0.1, 0.0, 1.0));
-  }
+    rgb = mix(rgb, clr_fog, clamp(dist/rmMaxDist - 0.1, 0.0, 1.0));
 
-  // gamma
-  rgb = pow(rgb, vec3(0.4545));
+    // gamma
+    // rgb = pow(rgb, vec3(0.4545));
+    rgb = pow(rgb, vec3(0.6));
   
-  // ray steps
-  // RGBA.r += numSteps / rmMaxSteps;
+    // ray steps
+    // RGBA.r += numSteps / rmMaxSteps;
 
-  RGBA.rgb = rgb;
+    RGBA.rgb = rgb;
 }
 
 
 // grimoire bindings
+#ifdef GRIMOIRE
 out vec4 fragColor; void main() { mainImage(fragColor, gl_FragCoord.xy); }
+#endif
+
